10bf3ccb1b8dcbfa2d446f9af8f5c5bdcc9dea98
impfundev <mail@ilhammaulana.me>
2024-06-05 23:45:12 +0700

    fix: app listen

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,10 @@
 const app = require("./app");
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
-  console.log(`Main API is on: http://localhost:5000/api/v1`);
-  /* eslint-enable no-console */
-});
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//   /* eslint-disable no-console */
+//   console.log(`Listening: http://localhost:${port}`);
+//   console.log(`Main API is on: http://localhost:5000/api/v1`);
+//   /* eslint-enable no-console */
+// });
+module.exports = app;
