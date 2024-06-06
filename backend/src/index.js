7092291b8fba3cee70e322482a3d14e6b8f88dc1
impfundev <mail@ilhammaulana.me>
2024-06-06 15:42:23 +0700

    fix: inactived app listen

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,11 @@
 const app = require("./app");
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
-  console.log(`Main API is on: http://localhost:${port}/api/v1`);
-  /* eslint-enable no-console */
-});
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   /* eslint-disable no-console */
+//   console.log(`Listening: http://localhost:${port}`);
+//   console.log(`Main API is on: http://localhost:${port}/api/v1`);
+//   /* eslint-enable no-console */
+// });
 
 module.exports = app;
